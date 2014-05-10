2b2cc06daed800fd81b5218341b17a93132b328a
Shane E <zcworld@gmail.com>
2014-05-10 22:57:19 +0930

    Update and rename u14.sh to u14_beta1.sh

--- u14_beta1.sh
+++ u14_beta1.sh
@@ -1,3 +1,69 @@
+#!/usr/bin/env bash
+
+# OS VERSION: Ubuntu Server 14.04.x LTS
+# ARCH: x32_64
+
+ZPX_VERSION=10.1.1
+
+# UNOfficial ZPanel Automated Installation Script
+# =============================================
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+# First we check if the user is 'root' before allowing installation to commence
+if [ $UID -ne 0 ]; then
+    echo "Install failed! To install you must be logged in as 'root', please try again."
+    exit 1
+fi
+
+# Lets check for some common control panels that we know will affect the installation/operating of ZPanel.
+if [ -e /usr/local/cpanel ] || [ -e /usr/local/directadmin ] || [ -e /usr/local/solusvm/www ] || [ -e /usr/local/home/admispconfig ] || [ -e /usr/local/lxlabs/kloxo ] ; then
+    echo "You appear to have a control panel already installed on your server; This installer"
+    echo "is designed to install and configure ZPanel on a clean OS installation only!"
+    echo ""
+    echo "Please re-install your OS before attempting to install using this script."
+    exit
+fi
+
+# Lets check for some common packages that we know will affect the installation/operating of ZPanel.
+# We expect a clean OS so no apache/mySQL/bind/postfix/php!
+if dpkg -s php apache mysql bind postfix dovecot; then
+    echo "You appear to have a server with apache/mysql/bind/postfix already installed; "
+    echo "This installer is designed to install and configure ZPanel on a clean OS "
+    echo "installation only!"
+    echo ""
+    echo "Please re-install your OS before attempting to install using this script."
+    exit
+fi
+
+# Ensure the installer is launched and can only be launched on Ubuntu 12.04
+BITS=$(uname -m | sed 's/x86_//;s/i[3-6]86/32/')
+if [ -f /etc/lsb-release ]; then
+  OS=$(cat /etc/lsb-release | grep DISTRIB_ID | sed 's/^.*=//')
+  VER=$(cat /etc/lsb-release | grep DISTRIB_RELEASE | sed 's/^.*=//')
+else
+  OS=$(uname -s)
+  VER=$(uname -r)
+fi
+echo "Detected : $OS  $VER  $BITS"
+if [ "$OS" = "Ubuntu" ] && [ "$VER" = "14.04" ]; then
+  echo "Ok."
+else
+  echo "Sorry, this installer only supports the installation of ZPanel on Ubuntu 12.04."
+  exit 1;
+fi
 
 ZPX_VERSION=10.1.1
 
@@ -15,8 +81,9 @@ passwordgen() {
 # Display the 'welcome' splash/user warning info..
 echo -e ""
 echo -e "##############################################################"
+echo -e "# UNOfficial ZPanel Automated Installation Script BETA 1     #"
 echo -e "# Welcome to the Official ZPanelX Installer for Ubuntu       #"
-echo -e "# Server 12.04.x LTS                                         #"
+echo -e "# Server 14.04.x LTS                                         #"
 echo -e "#                                                            #"
 echo -e "# Please make sure your VPS provider hasn't pre-installed    #"
 echo -e "# any packages required by ZPanelX.                          #"
@@ -89,27 +156,17 @@ dpkg --get-selections
 
 # We need to update the enabled Aptitude repositories
 echo -ne "\nUpdating Aptitude Repos: " >/dev/tty
-#if grep -Fxq "deb-src" /etc/apt/sources.list
-#then
-#    echo "sources list up-to-date"
-#else
-#    echo "deb-src http://archive.ubuntu.com/ubuntu precise main" >> /etc/apt/sources.list
-#    echo "deb-src http://archive.ubuntu.com/ubuntu precise-updates main" >> /etc/apt/sources.list
-#    echo "deb-src http://security.ubuntu.com/ubuntu precise-security main" >> /etc/apt/sources.list
-#    echo "deb-src http://archive.ubuntu.com/ubuntu precise universe" >> /etc/apt/sources.list
-#    echo "deb-src http://archive.ubuntu.com/ubuntu precise-updates universe" >> /etc/apt/sources.list
-#fi
-#to avoid compatibility problems have ppa and removes deposits in the outcry over
- mkdir -p "/etc/apt/sources.list.d.save"
+
+mkdir -p "/etc/apt/sources.list.d.save"
         cp -R "/etc/apt/sources.list.d/*" "/etc/apt/sources.list.d.save" &> /dev/null
         rm -rf "/etc/apt/sources.list/*"
         cp "/etc/apt/sources.list" "/etc/apt/sources.list.save"
 cat > /etc/apt/sources.list <<EOF
 #Dépots main restricted
 
-deb http://192.168.137.239/ubuntu trusty main restricted universe multiverse
-deb http://192.168.137.239/ubuntu trusty-security main restricted universe multiverse
-deb http://192.168.137.239/ubuntu trusty-updates main restricted universe multiverse
+deb http://archive.ubuntu.com/ubuntu trusty main restricted universe multiverse
+deb http://archive.ubuntu.com/ubuntu trusty-security main restricted universe multiverse
+deb http://archive.ubuntu.com/ubuntu trusty-updates main restricted universe multiverse
 
 EOF
 
@@ -383,7 +440,7 @@ echo -e "##############################################################" &>/dev/
 echo -e "" &>/dev/tty
 
 # We now request that the user restarts their server...
-read -e -p "Restart your server now to complete the install (y/n)? " rsn
+read -e -p "You Must Restart your server now to complete the install (y/n)? " rsn
 while true; do
 	case $rsn in
 		[Yy]* ) break;;
